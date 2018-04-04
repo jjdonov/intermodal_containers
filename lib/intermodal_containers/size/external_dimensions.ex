9125eb4fe5df47439cd9e181427be4a2caec76f5
John Donovan <jjdonov@gmail.com>
2018-04-03 22:38:49 -0700

    dims struct

---
+++ lib/intermodal_containers/size/external_dimensions.ex
@@ -0,0 +1,3 @@
+defmodule IntermodalContainers.Size.ExternalDimensions do
+  defstruct [:length, :width, :height]
+end
--- lib/intermodal_containers/size/parser.ex
+++ lib/intermodal_containers/size/parser.ex
@@ -3,6 +3,7 @@ defmodule IntermodalContainers.Size.Parser do
   alias IntermodalContainers.Size.Length
   alias IntermodalContainers.Size.Width
   alias IntermodalContainers.Size.Height
+  alias IntermodalContainers.Size.ExternalDimensions
 
   #TODO: error handling
 
@@ -10,7 +11,7 @@ defmodule IntermodalContainers.Size.Parser do
     {first, second} = String.split_at(code, 1)
     length = parse_l(first)
     {width, height} = parse_wh(second)
-    %{length: length, width: width, height: height}
+    %ExternalDimensions{length: length, width: width, height: height}
   end
 
   defp parse_l(code) when is_binary(code) and byte_size(code) == 1 do
