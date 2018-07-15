fb4187d3a6320dc06ef272f998e7f83653fd1823
John Donovan <jjdonov@gmail.com>
2018-07-14 21:59:01 -0700

    refactors to allow w&h coming from the same token

--- lib/intermodal_containers/coding/type_parser.ex
+++ lib/intermodal_containers/coding/type_parser.ex
@@ -1,13 +1,10 @@
 defmodule IntermodalContainers.Coding.Parser do
 
-  alias IntermodalContainers.Coding.SizeCodes
   alias IntermodalContainers.ParseError
 
   alias IntermodalContainers.Coding.DooDoo
-
+  alias IntermodalContainers.Coding.SizeCodes
   alias IntermodalContainers.Coding.TypeCodes
-
-  #use IntermodalContainers.SerialParser
 
   def parse(code) do
     try do
@@ -21,57 +18,85 @@ defmodule IntermodalContainers.Coding.Parser do
     parse_step({code, 0, %DooDoo{}})
   end
 
-  defp parse_step({_code, 0, %{}} = parse_state) do
-    validator = validateSize(:length)
-    consume(parse_state, 1, validator ) # fn s -> %{ s |  }) #idea: if we pass updateFn
+  defp parse_step({_rest, 0, %{}} = parse_state) do
+    consume(parse_state, 1, &accept_l/1 ) # fn s -> %{ s |  }) #idea: if we pass updateFn
   end
 
-  defp parse_step({_code, 1, %{}} = parse_state) do
-    validator = validateSize(:width)
-    consume(parse_state, 1, validator)
+  defp parse_step({_rest, 1, %{}} = parse_state) do
+    consume(parse_state, 1, &accept_w_h/1)
   end
 
-  defp parse_step({_code, 1, _} = parse_state) do
-    validator = validateSize(:height)
-    consume(parse_state, 1, validator)
-  end
-
-  defp parse_step({_code, 2, _} = parse_state) do
-    validator = &validate(:type, 2, &1)
-    consume(parse_state, 2, validator)
+  defp parse_step({_rest, 2, _} = parse_state) do
+    consume(parse_state, 2, &accept_type/1)
   end
 
   defp parse_step({_, 4, result}), do: result
 
-  defp validateSize(key), do: &validate(key, 1, &1)
-
-  defp validate(key, size, code) do
-    cond do byte_size(code) != size ->
-        {:error, "size code should be #{size} characters, got #{byte_size(code)}"}
-      lookup(key, code) == nil ->
-        {:error, "no #{Atom.to_string(key)}code for #{code}"}
+  defp accept_l(size_code) do
+    l = lookup(:length, size_code)
+    cond do
+      byte_size(size_code) != 1 ->
+        {:error, "length code should be 1 character long. #{size_code} is #{byte_size(size_code)}"}
+      lookup(:length, size_code) == nil ->
+        {:error, "unrecognized length code"}
       true ->
-        {:ok, key, code}
+        {:ok,  &update_state(&1, [length: l])}
     end
   end
+
+  defp accept_w_h(size_code) do
+    w = lookup(:width, size_code)
+    h = lookup(:height, size_code)
+    cond do
+      byte_size(size_code) != 1 ->
+        {:error, "width height code should be 1 character long"}
+      w == nil ->
+        {:error, "unrecognixed width code"}
+      h == nil ->
+        {:error, "unrecognized height code"}
+      true ->
+        {:ok,  &update_state(&1, width: w, height: h)}
+    end
+  end
+
+  defp accept_type(type_code) do
+    type = lookup(:type, type_code)
+    cond do byte_size(type_code) != 2 ->
+        {:error, "type code should be 2 characters, got #{byte_size(type_code)}"}
+      type == nil ->
+        {:error, "unrecognized type code"}
+      true ->
+        {:ok, &update_state(&1, type: type)}
+    end
+  end
+
+  defp update_state(%DooDoo{} = state, key, code) do
+    Map.update!(state, key, fn _old -> code end)
+  end
+
+  defp update_state(%DooDoo{} = state, [{key, code} | rest]) do
+    update_state(state, key, code)
+    |> update_state(rest)
+  end
+
+  defp update_state(%DooDoo{} = state, []),do: state
 
   defp lookup(:length, code), do: SizeCodes.get_length(code)
   defp lookup(:width,  code), do: SizeCodes.get_width(code)
   defp lookup(:height, code), do: SizeCodes.get_height(code)
   defp lookup(:type, code), do: TypeCodes.get(code)
 
-   def consume({code, position, res}, size, validator) do
+   def consume({code, position, res}, size, acceptFn) do
      {target, rest} = String.split_at(code, size)
 
-     validator.(target)
+     acceptFn.(target)
      |> advance(rest, position + size, res)
    end
 
    def advance({:error, _reason} = err, _code, _position, _parsed_container_number), do: err
 
-   def advance({:ok, key, val}, remainder, next_position, intermediate_result) do
-     intermediate_result = Map.update!(intermediate_result, key, fn _old -> val end)
-     parse_step({remainder, next_position, intermediate_result})
+   def advance({:ok, updateFn}, remainder, next_position, intermediate_result) do
+     parse_step({remainder, next_position, updateFn.(intermediate_result)})
    end
 
 end
