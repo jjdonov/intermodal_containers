37b33aac81b59d00a29c90c12b5f731c3bdbb8ed
John Donovan <jjdonov@gmail.com>
2019-05-20 23:56:07 -0700

    Configures doctest for top level IntermodalContainers module

--- lib/intermodal_containers.ex
+++ lib/intermodal_containers.ex
@@ -16,6 +16,17 @@ defmodule IntermodalContainers do
   Parse a container number string.
   Returns an `IntermodalContainers.ContainerNumber` struct.
 
+  ## Examples
+
+  iex> IntermodalContainers.parse_container_number("CSQU3054383")
+  {:ok,
+   %IntermodalContainers.ContainerNumber{
+    category_identifier: "U",
+    check_digit: "3",
+    owner_code: "CSQ",
+    serial_number: "305438"
+  }}
+
   """
   def parse_container_number(container_number) do
     ContainerNumber.parse(container_number)
@@ -23,6 +34,12 @@ defmodule IntermodalContainers do
 
   @doc """
   Performs checksum on the given container number.
+
+  ## Examples
+
+  iex> IntermodalContainers.checksum("CSQU3054383")
+  true
+
   """
   def checksum(container_number) do
     Checksum.check(container_number)
@@ -34,6 +51,39 @@ defmodule IntermodalContainers do
 
   Semantic meaning of the consituent parts of the code
   are provided by the struct.
+
+  ## Examples
+
+  iex> IntermodalContainers.parse_container_code("20GP")
+  {:ok,
+   %IntermodalContainers.ContainerCode{
+     height: %IntermodalContainers.ContainerCode.SimpleCode{
+       code_character: "0",
+       inches: 96,
+       is_unassiged: false,
+       mm: 2438
+     },
+     length: %IntermodalContainers.ContainerCode.SimpleCode{
+       code_character: "2",
+       inches: 240,
+       is_unassiged: false,
+       mm: 6068
+     },
+     type: %IntermodalContainers.ContainerCode.TypeCode{
+       characteristics: nil,
+       code: "G",
+       designation: "General purpose container without ventilation",
+       detailed_type_code: nil,
+       group_code: "GP"
+     },
+     width: %IntermodalContainers.ContainerCode.SimpleCode{
+       code_character: "0",
+       inches: nil,
+       is_unassiged: false,
+       mm: 2438
+     }
+   }}
+
   """
   def parse_container_code(container_code) do
     ContainerCode.parse(container_code)
---
+++ test/intermodal_containers/intermodal_containers_test.exs
@@ -0,0 +1,4 @@
+defmodule IntermodalContainersTest do
+  use ExUnit.Case
+  doctest IntermodalContainers
+end
